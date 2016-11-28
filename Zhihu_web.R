3529c6ef0aa0b2c58169ba3572b54ef87203d7cd
Chuyue Huang <chuyue.huang@berkeley.edu>
2016-11-28 14:52:06 -0800

    First Commit

---
+++ Zhihu_web.R
@@ -0,0 +1,8 @@
+## PS 239T Webscrapping Zhihu
+## author: Chuyue Frances Huang 
+## date: November 28, 2016
+
+######################
+### setup ##############
+source("/Users/Chuyue/Google Drive/Berkeley/Rye Pie/R Packages.R")
+setwd("/Users/Chuyue/Google Drive/Berkeley/Fall 2016/Academics/PS 239T/Final Project")
